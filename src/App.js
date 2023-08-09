8869bf803dbac24a296d8d9212e1825150e512cf
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2023-08-09 20:19:55 +0200

    result moved to Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import Form from "./Form";
-import { useState } from 'react';
-import currencies from "./Form/currencies.js";
+
 
 function App() {
-  const [result, setResult] = useState("");
-
-  const finalResult = (currency, amount) => {
-    const rate = currencies.find(({ shortcut }) => shortcut === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount * rate,
-      currency,
-    });
-  };
 
   return (
     <div>
-      <Form
-        finalResult={finalResult}
-        result={result}
-      />
+      <Form/>
     </div>
   );
 }
