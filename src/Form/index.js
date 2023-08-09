db8aa7349741f50bb2f99e5572a88df3c0f8a68a
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2023-08-09 20:20:34 +0200

    status added to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,38 @@
 import { useState } from 'react';
-import currencies from "./currencies";
 import Result from "../Result";
 import { Clock } from "../Clock";
-import { Container, Legend, Label, Select, Input, Fieldset, Span, Pharagraph, Button, InputLabel, FormBody } from "./styled"
+import {
+  Container,
+  Legend,
+  Label,
+  Select,
+  Input,
+  Fieldset,
+  Span,
+  Pharagraph,
+  Button,
+  InputLabel,
+  FormBody, 
+  Loading,
+} from "./styled"
+import { useData } from "../useData";
 
-const Form = ({ finalResult, result }) => {
+const Form = () => {
   const [amount, setAmount] = useState("")
-  const [currency, setCurrency] = useState(currencies[0].shortcut);
+  const [currency, setCurrency] = useState("EUR");
+  const [result, setResult] = useState("");
+  const ratesData = useData();
+
+
+  const finalResult = (currency, amount) => {
+    const rate = ratesData.rates[currency];
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -19,42 +45,65 @@ const Form = ({ finalResult, result }) => {
         <Clock />
         <Fieldset>
           <Legend>Currency calculator</Legend>
-          <p>
-            <Label>
-              <span>Currency*</span>
-              <Select
-                value={currency}
-                onChange={({ target }) => setCurrency(target.value)}
-              >
-                {currencies.map(currency => (
-                  <option
-                    key={currency.shortcut}
-                    value={currency.shortcut}
-                  >
-                    {currency.shortcut}
-                  </option>
-                ))}
-              </Select>
-            </Label>
-          </p>
-          <p>
-            <InputLabel>
-              <Span>Amount*</Span>
-              <Input
-                type="number"
-                minLength="1"
-                step="0.01"
-                required
-                value={amount}
-                placeholder="Put the amount"
-                min="0.01"
-                onChange={({ target }) => setAmount(target.value)}
-              />
-            </InputLabel>
-          </p>
-          <Button>Convert currency to PLN</Button>
-          <Pharagraph>Exchange rates are from google finance dated on 04.06.2023</Pharagraph>
-          <Result result={result} />
+          {ratesData.status === "loading"
+            ?
+            (
+              <Loading>
+                Please be patient, loading exchange rates
+              </Loading>
+            )
+            :
+            ratesData.status === "error"
+              ?
+              (
+                <p>
+                  Error, something went wrong. Please check your network or try later
+                </p>
+              )
+              :
+              (
+                <>
+                  <p>
+                    <Label>
+                      <span>Currency*</span>
+                      <Select
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
+                      >
+                        {Object.keys(ratesData.rates).map(currency => (
+                          <option
+                            key={currency}
+                            value={currency}
+                          >
+                            {currency}
+                          </option>
+                        ))}
+                      </Select>
+                    </Label>
+                  </p>
+                  <p>
+                    <InputLabel>
+                      <Span>Amount*</Span>
+                      <Input
+                        type="number"
+                        minLength="1"
+                        step="0.01"
+                        required
+                        value={amount}
+                        placeholder="Put the amount"
+                        min="0.01"
+                        onChange={({ target }) => setAmount(target.value)}
+                      />
+                    </InputLabel>
+                  </p>
+                  <Button>Convert currency to PLN</Button>
+                  <Pharagraph>Exchange rates are from exchangerate.host dated on {ratesData.date}
+                  </Pharagraph>
+                  <Result result={result} />
+                </>
+              )
+          }
+
         </Fieldset>
       </FormBody>
     </Container>
